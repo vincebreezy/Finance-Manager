4b2b3e08046c6c80a589d86477562b09a9d19897
kambati067 <ketanreddy.ambati@sjsu.edu>
2023-09-26 18:21:36 -0700

    login basics

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,8 +1,21 @@
-from flask import Flask
-
+from flask import Flask, render_template, request, redirect, url_for, session
+import sqlite3
 app = Flask(__name__)
+app.secret_key = 'finance_manager'
+
+db = con.cursor("CREATE TABLE user(user_id, username, password, )")
 
 #members API route
+def create_connection():
+	con = sqlite3.connect("finance.db")
+	return con
+@app.route('/')
+def home():
+	if 'username' in sessions:
+		#route to dashboard
+	else:
+		#route the login
+
 
 @app.route("/members")
 def members():
